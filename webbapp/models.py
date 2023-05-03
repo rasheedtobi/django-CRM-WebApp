5ae50b15a663ff18b3bc1ee7ec79e8be8b15fe3c
Rasheed Odunbaku <tobirasheed@gmail.com>
2023-05-03 15:29:06 +0200

    all records displaying

--- webbapp/models.py
+++ webbapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Record(models.Model):
+    created_time = models.DateTimeField(
+        auto_now_add=True, null=True, blank=True)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    phone_no = models.IntegerField()
+    address = models.CharField(max_length=200)
+    zip_code = models.IntegerField()
+    state = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name + ' ' + self.state
---
+++ webbapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+# app_name = 'webbapp'
+urlpatterns = [
+    path('', views.home, name='home'),
+    # path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('register/', views.register_user, name='register'),
+
+
+]
