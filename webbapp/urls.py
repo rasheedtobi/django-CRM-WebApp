3753a629873020267ea93375501bf1c465cd5e14
Rasheed Odunbaku <tobirasheed@gmail.com>
2023-07-25 16:10:07 +0200

    Final

--- webbapp/urls.py
+++ webbapp/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('register/', views.register_user, name='register'),
-
+    path('record/<int:pk>', views.record, name='record'),
+    path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
+    path('add_new_record/', views.add_new_record, name='add_new_record'),
+    path('update_record/<int:pk>', views.update_record, name='update_record'),
 
 ]
--- webbapp/views.py
+++ webbapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 # Create your views here.
@@ -51,3 +51,61 @@ def register_user(request):
         form = SignUpForm()
         return render(request, 'register.html', {'form': form})
     return render(request, 'register.html', {'form': form})
+
+
+def record(request, pk):
+    if request.user.is_authenticated:
+        # Chech record
+        record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'record': record})
+
+    else:
+        messages.success(
+            request, "You must be logged in to view access individual records")
+        return redirect('home')
+
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        # Chech record
+        del_record = Record.objects.get(id=pk)
+        del_record.delete()
+        messages.success(request, "You have  successfully deleted the record")
+        return redirect('home')
+    else:
+        messages.success(
+            request, "You must be logged in to be able  delete a record")
+        return redirect('home')
+
+
+def add_new_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if form.is_valid():
+                form.save()
+                messages.success(
+                    request, "You have  successfully added a new record")
+                return redirect('home')
+        return render(request, 'add_new_record.html', {'form': form})
+    else:
+        messages.success(
+            request, "You must be logged in to be able  add a record")
+        return redirect('home')
+
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        # Chech record
+        upd_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=upd_record)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, "You have  successfully updated the record")
+            return redirect('home')
+        return render(request, 'update_record.html', {'form': form})
+    else:
+        messages.success(
+            request, "You must be logged in to be able  update a record")
+        return redirect('home')
